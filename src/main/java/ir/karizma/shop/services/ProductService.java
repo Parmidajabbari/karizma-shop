722c34252337cd9e54ad011e8ad6b0e68d99464c
Parmida Jabbari <parmidajabbari@gmail.com>
2023-06-09 19:47:54 +0330

    feat: add product service and repository

---
+++ src/main/java/ir/karizma/shop/services/ProductService.java
@@ -0,0 +1,33 @@
+package ir.karizma.shop.services;
+
+import ir.karizma.shop.domain.*;
+import ir.karizma.shop.repositories.ProductRepository;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Service
+public class ProductService {
+    private final ProductRepository productRepository;
+
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+    public Product getProduct(Long id) {
+        return productRepository.findById(id).orElse(null);
+    }
+
+    public Product saveProduct(Product product) {
+        return productRepository.save(product);
+    }
+
+    public void deleteProduct(Long id) {
+        productRepository.deleteById(id);
+    }
+}
